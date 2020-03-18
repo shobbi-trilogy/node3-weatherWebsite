2f3dcd7b38fcf1148829bcde3d4c8d8961d0996f
Sal Hobbi <shobbi@trilogyed.com>
2020-03-18 12:24:04 -0400

    removed unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('clientside javascript file is loaded....')
+//console.log('clientside javascript file is loaded....')
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
 //         console.log(data)
